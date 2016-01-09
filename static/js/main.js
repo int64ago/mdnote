350a08b5c9c666f395c73f7c2ee227525135da8f
Cody Chan <int64ago@gmail.com>
2016-01-09 18:20:30 +0800

    Enable copy url && text

--- static/js/main.js
+++ static/js/main.js
@@ -54,6 +54,35 @@
         }
     });
 
+    // Clipboard settings
+    var textClipboard = new Clipboard('#copy-text');
+    textClipboard.on('success', function(e) {
+        $('#copy-status').attr('data-text', '√');
+        setTimeout(function() {
+            $('#copy-status').attr('data-text', '-');
+        }, 1000);
+    });
+    textClipboard.on('error', function(e) {
+        $('#copy-status').attr('data-text', 'x');
+        setTimeout(function() {
+            $('#copy-status').attr('data-text', '-');
+        }, 1000);
+    });
+    $('#copy-url').attr('data-clipboard-text', window.location.href);
+    var urlClipboard = new Clipboard('#copy-url');
+    urlClipboard.on('success', function(e) {
+        $('#copy-status').attr('data-text', '√');
+        setTimeout(function() {
+            $('#copy-status').attr('data-text', '-');
+        }, 1000);
+    });
+    urlClipboard.on('error', function(e) {
+        $('#copy-status').attr('data-text', 'x');
+        setTimeout(function() {
+            $('#copy-status').attr('data-text', '-');
+        }, 1000);
+    });
+
     setInterval(function() {
         if (downCount > 0) {
             downCount -= 1;
